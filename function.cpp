fb5fe6cca384d11f4b8f3aec0ec540a7716bce07
ludo <tt982914@gmail.com>
2024-12-27 00:06:18 +0100

    changement de pc

--- function.cpp
+++ function.cpp
@@ -36,14 +36,54 @@ void addForAllNCoord(vector<vector<int>> &points, vector<int> center, int nbArou
 
 void addAllVarriationOfCoord(vector<vector<int>> &point, vector<int> center, vector<int> coord) {
     // ajoute les points définit par the coord (but the coord is not always the in the right dimention)
+    // abc -> abcaa abcbb abccc abcab abcac abcbc
+     
+}
+
+bool getNextVarriation(vector<int> &coord, int idxStart, int nbAround) {
+    // modifie et renvoie true les coord pour avoir la varriation d'appès.
+    // abc -> abcaa abcbb abccc abcab abcac abcbc
+    int idx = coord.size()-1;
+    while (coord[idx] == nbAround) {
+        if (idx == idxStart) return false;
+        idx --;
+    }
+    coord[idx] ++;
+    int res = coord[idx];
+    idx++;
+    while (idx!=coord.size()) {
+        coord[idx] = res;
+        idx++;
+    }
+    return true;
 }
 
 void addIfInScop(vector<vector<int>> &points, vector<int> center, vector<int> coord) {
     // ajoute un un point si il n'est pas en dehors de l'espace (retour au subSapce fin du théorique)
-
+     
 }
 
 bool updateVectorIfUpdatable(vector<int> &coord, int nbAround) {
     // modifie les veleur de coordonné jusqu'au max de nbAround puis renvoit false quand il ne peut plus
-    
+    // abc abd acd bcd
+    // ab ac ad bc bd cd
+    int idx = coord.size()-1;
+    while (coord[idx] == nbAround) {
+        if (idx == 0) return false;
+        idx --;
+    }
+    while (coord[idx] + idx - coord.size() > nbAround) {
+        if (idx == 0) return false;
+        idx --;
+    }
+    coord[idx] ++;
+    int res = coord[idx];
+    idx++;
+    res ++;
+    while (idx!=coord.size()) {
+        coord[idx] = res;
+        idx++;
+        res++;
+    }
+    return true;
 }
--- function.hpp
+++ function.hpp
@@ -9,4 +9,5 @@ vector<vector<int>> findcoordAround(vector<int> center, int nbAround);
 void addForAllNCoord(vector<vector<int>> &points, vector<int> center, int nbAround, int nbCoord);
 bool updateVectorIfUpdatable(vector<int> &coord, int nbAround);
 void addAllVarriationOfCoord(vector<vector<int>> &points, vector<int> center, vector<int> coord);
+bool getNextVarriation(vector<int> &coord,int idxStart,int nbAround);
 void addIfInScop(vector<vector<int>> &points, vector<int> center, vector<int> coord);
